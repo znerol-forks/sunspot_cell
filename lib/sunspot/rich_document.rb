c46a1477a14ef953281ae94ce9c76daf3e8d0e2f
Codie Mullins <codie.mullins@waterfield.com>
2013-10-31 15:50:34 -0500

    Revert "Modify fmap.content to equal "document_text""
    This reverts commit dfcd5ba0c3188fbf70890c3c5f2eda99bbb5bbff.

--- lib/sunspot/rich_document.rb
+++ lib/sunspot/rich_document.rb
@@ -21,7 +21,7 @@ module Sunspot
 
       @fields.each do |f|
         if f.name.to_s.include?("_attachment") and f.value.present?
-          params['fmap.content'] = "document_text"
+          params['fmap.content'] = "attr_content"
           if f.value.is_a?(Hash)
             params['stream.url']         = f.value[:file]
             params['stream.contentType'] = f.value[:type]
